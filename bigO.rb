e86fc146689acfdb52c89db241e9f4563f3aa584
Ryder_Aguilera <ryderaguilera@hotmail.com>
2023-02-24 11:57:12 -0800

    Execution time exercises

---
+++ bigO.rb
@@ -0,0 +1,74 @@
+#
+
+def my_min(list)
+
+    list.each do |num|
+        return num if list.all? {|num2| num <= num2}
+    end
+end
+
+# O(n^2)
+p my_min([0,3,5,4,-5,10])
+
+def my_min_better(list)
+
+    min = 10000
+
+    list.each do |num|
+        min = num if num <= min 
+    end
+    min 
+
+end
+
+#O(n)
+p my_min_better([0,3,5,4,-5,10])
+
+def large_cont_subsum(list) 
+    sub_arrys = []
+
+    list.each_with_index do |ele,i|
+        sub_arrys << [ele]
+        list.each_with_index do |ele1,i1|
+            if i1 > i 
+                sub_arrys << list[i..i1]
+            end
+        end
+    end
+    max = 0
+    sub_arrys.each do |sum|
+        max = sum.sum if sum.sum > max 
+    end
+    max 
+
+end
+list = [-2,3,-6,7,-6,7]
+p large_cont_subsum(list)
+# winner = -2
+# current = -2 
+# either current = 1 or current = 3
+def bet_large_cont_subsum(list)
+    winner = -1000
+    current = 0 
+    # -6, so current becomes -3, and winner is still 3
+    # 7, so current is 4, but 4 is worse than 7 so current is 7 and winner is 7
+    list.each_with_index do |num|
+        current += num #1
+        current = num if num > current # 3 > 1, forget adding, lets use 3 instead
+        winner = current if current > winner #Now swap winner to 3.
+    end
+    winner 
+end
+     
+p bet_large_cont_subsum(list)
+
+
+
+        
+
+        
+
+
+
+
+
