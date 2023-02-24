7597102d220ab6403f817f0a10e66389dcf6a7ee
chrisbanas <bananas595@gmail.com>
2023-02-24 15:06:00 -0800

    Started window max range

--- bigO.rb
+++ bigO.rb
@@ -1,5 +1,7 @@
 #
 
+require "byebug"
+
 def my_min(list)
 
     list.each do |num|
@@ -8,7 +10,7 @@ def my_min(list)
 end
 
 # O(n^2)
-p my_min([0,3,5,4,-5,10])
+# p my_min([0,3,5,4,-5,10])
 
 def my_min_better(list)
 
@@ -22,7 +24,7 @@ def my_min_better(list)
 end
 
 #O(n)
-p my_min_better([0,3,5,4,-5,10])
+# p my_min_better([0,3,5,4,-5,10])
 
 def large_cont_subsum(list)
     sub_arrys = []
@@ -43,7 +45,7 @@ def large_cont_subsum(list)
 
 end
 list = [-2,3,-6,7,-6,7]
-p large_cont_subsum(list)
+# p large_cont_subsum(list)
 # winner = -2
 # current = -2
 # either current = 1 or current = 3
@@ -60,7 +62,7 @@ def bet_large_cont_subsum(list)
     winner
 end
 
-p bet_large_cont_subsum(list)
+# p bet_large_cont_subsum(list)
 
 
 def first_anagram?(str1, str2)
@@ -102,8 +104,8 @@ def second_anagram?(str1, str2)
 
 end
 
-p second_anagram?("gizmogizmogizmogizmo", "sallysallysallysally")    #=> false
-p second_anagram?("elviselviselviselvis", "liveslivesliveslives")    #=> true
+# p second_anagram?("gizmogizmogizmogizmo", "sallysallysallysally")    #=> false
+# p second_anagram?("elviselviselviselvis", "liveslivesliveslives")    #=> true
 
 # p first_anagram?("gizmogizmo", "sallysally")    #=> false
 # p first_anagram?("elviselvis", "liveslives")    #=> true
@@ -111,21 +113,21 @@ p second_anagram?("elviselviselviselvis", "liveslivesliveslives")    #=> true
 def third_anagram?(str1,str2)
     chars1 = str1.chars
     # We have spent n time O(n)
-    sort1 = chars1.sort 
+    sort1 = chars1.sort
     # Add n log n time
     chars2 = str2.chars
     # add n time
-    sort2 = chars2.sort 
+    sort2 = chars2.sort
     # add n log n time
-    sort1 == sort2 
+    sort1 == sort2
     # add 1
 
-    # n + nlogn + n + nlogn => 2n + 2nlogn -> 2n (1 + logn) 
+    # n + nlogn + n + nlogn => 2n + 2nlogn -> 2n (1 + logn)
     # n log n
-end 
+end
 
-p third_anagram?("gizmogizmogizmogizmo", "sallysallysallysally")    #=> false
-p third_anagram?("elviselviselviselvis", "liveslivesliveslives")    #=> true
+# p third_anagram?("gizmogizmogizmogizmo", "sallysallysallysally")    #=> false
+# p third_anagram?("elviselviselviselvis", "liveslivesliveslives")    #=> true
 # Time time complexity is nlogn
 
 def fourth_anagram?(str1,str2)
@@ -139,11 +141,145 @@ def fourth_anagram?(str1,str2)
         hash[char1] -= 1
     end
 
-    hash.each do |k,v| 
+    hash.each do |k,v|
         return false if v != 0
     end
     true
 end
 
-p fourth_anagram?("gizmogizmogizmogizmo", "sallysallysallysally")    #=> false
-p fourth_anagram?("elviselviselviselvis", "liveslivesliveslives")    #=> true
+# p fourth_anagram?("gizmogizmogizmogizmo", "sallysallysallysally")    #=> false
+# p fourth_anagram?("elviselviselviselvis", "liveslivesliveslives")    #=> true
+
+
+def bad_two_sum?(arr, target_sum)
+
+    arr.each_with_index do |num1, i1|
+        arr.each_with_index do |num2, i2|
+            if i2 > i1 && num1 + num2 == target_sum
+                return true
+            end
+        end
+    end
+
+    false
+
+end
+
+# n^2
+
+arr = [0, 1, 5, 7]
+# p bad_two_sum?(arr, 6) # => should be true
+# p bad_two_sum?(arr, 10) # => should be false
+
+
+def okay_two_sum?(arr, target_sum)
+
+    sorted = arr.sort
+
+    sorted.each do |num|
+        pair = target_sum - num
+        next if pair == num
+        return true if sorted.bsearch { |x| x == pair }
+    end
+
+    false
+
+end
+
+# n log n
+
+arr = [0, 1, 5, 7]
+# p okay_two_sum?(arr, 6) # => should be true
+# p okay_two_sum?(arr, 10) # => should be false
+
+
+def hash_two_sum?(arr, target_sum)
+
+    hash = Hash.new(0)
+
+    arr.each do |num|
+        pair = target_sum - num
+        return true if hash[pair] > 0
+        hash[num] += 1
+    end
+
+    false
+end
+
+# O(n)
+
+# p hash_two_sum?(arr, 6) # => should be true
+# p hash_two_sum?(arr, 10) # => should be false
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+# step 1 define window max range. It will take in an array and sixe
+# step 2 find all the sub arrays of length size
+# the best sub array is the one of max - min of its values
+
+
+def naive_windowed_max_range(array, window_size)
+
+    current_max_range = nil
+
+debugger
+
+    array.each_with_index do |window, i|
+        next if (i + window_size - 1) >= array.length
+
+        sub_window = array[i...(i + window_size)]
+        max = sub_window.max
+        min = sub_window.min
+
+        if current_max_range.nil?
+            current_max_range = (max - min)
+        elsif (max - min) > current_max_range
+            current_max_range = (max - min)
+        end
+
+    end
+
+    current_max_range
+
+end
+
+# n^2
+
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 2) == 4 # 4, 8
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
+# p windowed_max_range([1, 3, 2, 5, 4, 8], 5) == 6 # 3, 2, 5, 4, 8
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+class MyQueue
+
+
+    def initialize
+        @store = []
+    end
+
+    def enqueue(ele)
+        @store << ele
+    end
+
+    def dequeue
+        @store.shift
+    end
+
+    def peek
+        @store.first
+    end
+
+    def size
+        @store.length
+    end
+
+    def empty?
+        @store.empty?
+    end
+
+end
